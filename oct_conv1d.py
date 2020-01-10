c38b69c65ed9cdec12cdfb4487554884cada3b70
liucy17 <wangxingyao@wangxingyaodeMacBook-Pro.local>
2020-01-10 09:53:10 +0800

    update in lab on 10/01

--- oct_conv1d.py
+++ oct_conv1d.py
@@ -1,9 +1,11 @@
 from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 import tensorflow.keras.backend as K
+from shift_conv1d import ShiftConv1D
+
 
 class OctConv1D(layers.Layer):
-    def __init__(self, filters, alpha, low_tau, high_tau, fs
+    def __init__(self, filters, alpha, beta, low_tau, high_tau, fs,
                  kernel_size=3, strides=1, padding='same',
                  kernel_initializer='glorot_uniform',
                  kernel_regularizer=None,
@@ -15,10 +17,11 @@ class OctConv1D(layers.Layer):
         super().__init__(**kwargs)
 
         self.alpha = alpha
+        self.beta = beta
         self.filters = filters
-        #self.low_tau = low_tau
-        #self.high_tau = high_tau
-        #self.fs = fs
+        self.low_tau = low_tau
+        self.high_tau = high_tau
+        self.fs = fs
         # optional values
         self.kernel_size = kernel_size
         self.strides = strides
@@ -27,53 +30,30 @@ class OctConv1D(layers.Layer):
         self.kernel_regularizer = kernel_regularizer
         self.kernel_constraint = kernel_constraint
 
+        # -> Low fs
+        self.low_fs = int(self.fs / self.alpha)
+        # -> High fs
+        self.high_fs = self.fs
+
         # -> Low channels
-        self.low_channels = int(self.filters * self.alpha)
+        self.low_channels = int(self.filters * self.beta)
         # -> high channels
         self.high_channels = self.filters - self.low_channels
 
         # -> Low shift length
-        # self.low_shift = self.low_tau * fs / 2
+        self.low_shift = self.low_tau * fs / self.alpha
         # -> High shift length
-        # `self.high_shift = self.high_tau * fs
+        self.high_shift = self.high_tau * fs
 
     def build(self, input_shape):
         assert len(input_shape) == 2
         assert len(input_shape[0]) == 3 and len(input_shape[1]) == 3
         # Assertion for high inputs
-        assert input_shape[0][1] // 2 >= self.kernel_size
+        assert input_shape[0][1] // self.alpha >= self.kernel_size
         # Assertion for low inputs
-        assert input_shape[0][1] // input_shape[0][1] == 2
+        assert input_shape[0][1] // input_shape[1][1] == self.alpha
         # channel last for Tensorflow
         assert K.image_data_format() == 'channels_last'
-        # input_channels
-        high_in = int(input_shape[0][2])
-        low_in = int(input_shape[1][2])
-
-        # High -> High
-        self.high_to_high_kernel = self.add_weight(name='high_to_high_kernel',
-                                                   shape=(*self.kernel_size, high_in, self.high_channels),
-                                                   initializer=self.kernel_initializer,
-                                                   regularizer=self.kernel_regularizer,
-                                                   constraint=self.kernel_constraint)
-        # High -> Low
-        self.high_to_low_kernel = self.add_weight(name="high_to_low_kernel",
-                                                  shape=(*self.kernel_size, high_in, self.low_channels),
-                                                  initializer=self.kernel_initializer,
-                                                  regularizer=self.kernel_regularizer,
-                                                  constraint=self.kernel_constraint)
-        # Low -> High
-        self.low_to_high_kernel = self.add_weight(name="low_to_high_kernel",
-                                                  shape=(*self.kernel_size, low_in, self.high_channels),
-                                                  initializer=self.kernel_initializer,
-                                                  regularizer=self.kernel_regularizer,
-                                                  constraint=self.kernel_constraint)
-        # Low -> Low
-        self.low_to_low_kernel = self.add_weight(name="low_to_low_kernel",
-                                                 shape=(*self.kernel_size, low_in, self.low_channels),
-                                                 initializer=self.kernel_initializer,
-                                                 regularizer=self.kernel_regularizer,
-                                                 constraint=self.kernel_constraint)
 
         super().build(input_shape)
 
@@ -82,23 +62,38 @@ class OctConv1D(layers.Layer):
         assert len(inputs) == 2
         high_input, low_input = inputs
         # High -> High conv
-        high_to_high = K.conv1d(high_input, self.high_to_high_kernel,
-                                strides=self.strides, padding=self.padding,
-                                data_format='channels_last')
+        high_to_high = ShiftConv1D(self.high_channels,
+                                   self.high_tau,
+                                   self.high_fs,
+                                   self.kernel_size,
+                                   strides=self.strides,
+                                   padding=self.padding)(high_input)
         # High -> Low conv
-        high_to_low = K.pool1d(high_input, 2, strides=2, pool_mode='max')
-        high_to_low = K.conv1d(high_to_low, self.high_to_low_kernel,
-                               strides=self.strides, padding=self.padding,
-                               data_format='channels_last')
+        high_to_low = ShiftConv1D(self.low_channels,
+                                  self.high_tau,
+                                  self.high_fs,
+                                  self.kernel_size,
+                                  strides=self.strides,
+                                  padding=self.padding)(high_input)
+        high_to_low = K.pool1d(high_to_low,
+                               self.alpha,
+                               strides=self.alpha,
+                               pool_mode='max')
         # Low -> High conv
-        low_to_high = K.conv1d(low_input, self.low_to_high_kernel,
-                               strides=self.strides, padding=self.padding,
-                               data_format='channels_last')
-        low_to_high = K.repeat_elements(low_to_high, axis=1)
+        low_to_high = ShiftConv1D(self.high_channels,
+                                  self.low_tau,
+                                  self.low_fs,
+                                  self.kernel_size,
+                                  strides=self.strides,
+                                  padding=self.padding)(low_input)
+        low_to_high = K.repeat_elements(low_to_high, self.alpha, axis=1)
         # Low -> Low conv
-        low_to_low = K.conv1d(low_input, self.low_to_low_kernel,
-                              strides=self.strides, padding=self.padding,
-                              data_format='channels_last')
+        low_to_low = ShiftConv1D(self.low_channels,
+                                 self.low_tau,
+                                 self.low_fs,
+                                 self.kernel_size,
+                                 strides=self.strides,
+                                 padding=self.padding)(low_input)
         # Cross Add
         high_add = high_to_high + low_to_high
         low_add = high_to_low + low_to_low
@@ -118,7 +113,10 @@ class OctConv1D(layers.Layer):
             **base_config,
             "filters": self.filters,
             "alpha": self.alpha,
-            "filters": self.filters,
+            "beta": self.beta,
+            "low_tau": self.low_tau,
+            "high_tau": self.high_tau,
+            "fs": self.fs,
             "kernel_size": self.kernel_size,
             "strides": self.strides,
             "padding": self.padding,
--- shift_conv1d.py
+++ shift_conv1d.py
@@ -3,6 +3,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Model
 import tensorflow.keras.backend as K
 
+
 class ShiftConv1D(layers.layer):
     def __init__(self, filters, tau, fs,
                  kernel_size=3, strides=1, padding='same',
@@ -25,7 +26,7 @@ class ShiftConv1D(layers.layer):
         self.kernel_constraint = kernel_constraint
 
         self.shift = int(self.tau * self.fs)
-        self.input_channels = int(input_shape[1] // self.shift)
+        self.input_channels = 3
 
     def build(self, input_shape):
         assert len(input_shape) == 3
@@ -34,15 +35,56 @@ class ShiftConv1D(layers.layer):
         # channel last for Tensorflow
         assert K.image_data_format() == 'channels_last'
 
-        self.kernel = self.add_weight(name='kernel',
-                                      shape=(*self.kernel_size, self.input_channels, self.filters),
-                                      initializer=self.kernel_initializer,
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint)
+        self.cross_kernel = self.add_weight(name='cross_kernel',
+                                            shape=(1,
+                                                   self.input_channels,
+                                                   self.filters),
+                                            initializer=self.kernel_initializer,
+                                            regularizer=self.kernel_regularizer,
+                                            constraint=self.kernel_constraint)
+        self.conv_kernel = self.add_weight(name='conv_kernel',
+                                           shape=(*self.kernel_size,
+                                                  self.input_channels,
+                                                  self.filters),
+                                           initializer=self.kernel_initializer,
+                                           regularizer=self.kernel_regularizer,
+                                           constraint=self.kernel_constraint)
 
         super().build(input_shape)
 
     def call(self, inputs):
         left_shift = tf.roll(inputs, self.shift, axis=1)
         right_shift = tf.roll(inputs, (-1)*self.shift, axis=1)
-        shift_inputs = tf.squeeze(tf.stack([inputs, left_shift, right_shift], axis=-2))
+        shift_inputs = tf.stack([inputs, left_shift, right_shift], axis=-2)
+        shift_inputs = tf.squeeze(shift_inputs)
+
+        cross_ch = K.conv1d(shift_inputs, self.cross_kernel,
+                            strides=1, padding=self.padding,
+                            data_format='channels_last')
+        shift_outputs = K.conv1d(cross_ch, self.conv_kernel,
+                                 strides=self.strides, padding=self.padding,
+                                 data_format='channels_last')
+
+        return shift_outputs
+
+    def compute_output_shape(self, input_shape):
+        out_shape = (*input_shape[: 2], self.filters)
+
+        return out_shape
+
+    def get_config(self):
+        base_config = super().get_config()
+        out_config = {
+            **base_config,
+            "filters": self.filters,
+            "tau": self.tau,
+            "fs": self.fs,
+            "kernel_size": self.kernel_size,
+            "strides": self.strides,
+            "padding": self.padding,
+            "kernel_initializer": self.kernel_initializer,
+            "kernel_regularizer": self.kernel_regularizer,
+            "kernel_constraint": self.kernel_constraint,
+        }
+
+        return out_config
